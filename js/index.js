468ab53ef2acf518a6cb8e76ed539b0f1f8f0a7a
pansonic2 <30543241+pansonic2@users.noreply.github.com>
2018-07-03 09:21:53 -0500

    WordSlam

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  
+$("#roll").on("click", showstuff)
+  function showstuff(){
+    
+    function randomFrom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+    allTiles = ['Kirill', 'planet', 'SpongeBob Squarepants', 'chainsaw', 'mayor', 'ballet', 'zipper', 'fog', 'Neil Armstrong', 'chicken', 'saliva', 'train station', 'tea', 'cap', 'river', 'candy', 'snowball', 'Terminator', 'mole', 'breakfast', 'Hollywood', 'GPS unit', 'Dora the Explorer', 'babysitter', 'banana', 'color', 'Albert Einstein', 'heavy metal', 'Olympic Games', 'pirate', 'school', 'elephant' ];
+    resultingTile = randomFrom(allTiles);
+    console.log(resultingTile);
+      
+    $("#result").text(resultingTile);
+  }});
